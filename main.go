03bad217f66397043f88896a12a43236efe095e0
Serge Voilokov <serge0x76@gmail.com>
2017-05-25 22:47:06 -0400

    Add handlers

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rand"
+	"crypto/tls"
+	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
@@ -27,15 +34,23 @@ var mainPageHTML = `
     vertical-align: middle;
     text-decoration: none;
     display: inline-block;
-    font-size: 22px;
+    font-size: 32px;
     width: 240px;
     height: 100px;
 }
 </style>
 </head>
 <body>
+	{{if .Error}}
+	<table border="0" style="width: 260px">
+		<tr><td style="color: red">ERROR: {{.Error}}</td><tr>
+		<tr><td><a class="button" href="/shutdown">Shutdown</a></td><tr>
+		<tr><td><a class="button" href="/restart">Restart</a></td><tr>
+		<tr><td><a class="button" href="/play">Test play</a></td><tr>
+	</table>
+	{{end}}
 	<table border="0">
-		{{range .}}
+		{{range .List}}
 		<tr><td><a class="button" href="/play?href={{.Href}}">{{.Name}}</a></td><tr>
 		{{end}}
 	</table>
@@ -43,7 +58,7 @@ var mainPageHTML = `
 </html>
 `
 
-var playPageHTML = `
+var genericPageHTML = `
 <html>
 <head>
 <title>Videos</title>
@@ -58,14 +73,17 @@ var playPageHTML = `
     vertical-align: middle;
     text-decoration: none;
     display: inline-block;
-    font-size: 22px;
+    font-size: 32px;
     width: 240px;
     height: 100px;
 }
 </style>
 </head>
 <body>
-	<table>
+	<table border="0" style="width: 260px">
+		{{if .Error}}
+		<tr><td style="color: red">ERROR: {{.Error}}</td><tr>
+		{{end}}
 		<tr><td><a href="/" class="button">Stop</a></td><tr>
 	</table>
 </body>
@@ -73,11 +91,16 @@ var playPageHTML = `
 `
 
 var mt = template.Must(template.New("main").Parse(mainPageHTML))
-var pt = template.Must(template.New("play").Parse(playPageHTML))
+var gt = template.Must(template.New("generic").Parse(genericPageHTML))
 
 type item struct {
 	Name string
 	Href string
+}
+
+type mainData struct {
+	Error error
+	List  []item
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
@@ -86,51 +109,96 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	stopPlayer()
-	list, err := getVideos()
-	if err != nil {
+
+	var d mainData
+	d.List, d.Error = getVideos()
+	if err := mt.Execute(w, d); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	if err := mt.Execute(w, list); err != nil {
+}
+
+type genericData struct {
+	Error error
+}
+
+func restartHandler(w http.ResponseWriter, r *http.Request) {
+	var d genericData
+	d.Error = exec.Command("sudo", "shutdown", "-r", "5").Run()
+	if err := gt.Execute(w, d); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	}
+}
+
+func shutdownHandler(w http.ResponseWriter, r *http.Request) {
+	var d genericData
+	d.Error = exec.Command("sudo", "shutdown", "-h", "5").Run()
+	if err := gt.Execute(w, d); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func playHandler(w http.ResponseWriter, r *http.Request) {
 	href := r.URL.Query().Get("href")
-	fmt.Println("starting player")
-	playVideo(href)
-	if err := pt.Execute(w, nil); err != nil {
+	log.Println("starting player")
+	var d genericData
+	d.Error = startPlayer(href)
+	if err := gt.Execute(w, d); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+type programState struct {
+	Elapsed map[string]int `json:"elapsed"`
+}
+
+var state = programState{
+	Elapsed: make(map[string]int),
+}
+
+func loadState() {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Get(confapi)
+	if err != nil {
+		log.Println("loadState", err.Error())
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Println("loadState status:", resp.StatusCode)
+		return
+	}
+	dec := json.NewDecoder(resp.Body)
+	defer resp.Body.Close()
+	if err := dec.Decode(&state); err != nil {
+		log.Println("loadState", err.Error())
 		return
 	}
 }
 
-func readInt(name string) int {
-	fname := fmt.Sprintf("cfg-%s.txt", name)
-	f, err := os.Open(fname)
-	if err != nil {
-		return 0
-	}
-	var n int
-	fmt.Fscan(f, &n)
-	f.Close()
-	n -= 10
-	if n < 0 {
-		n = 0
-	}
-	return n
-}
-
-func saveInt(name string, n int) {
-	fname := fmt.Sprintf("cfg-%s.txt", name)
-	f, err := os.Create(fname)
-	if err != nil {
+func saveState() {
+	var b bytes.Buffer
+	enc := json.NewEncoder(&b)
+	if err := enc.Encode(&state); err != nil {
+		log.Println(err)
 		return
 	}
-	fmt.Fprintf(f, "%d", n)
-	f.Close()
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Post(confapi, "application/json", &b)
+	if err != nil {
+		log.Println("saveState", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Println("saveState status", resp.Status)
+		return
+	}
 }
 
 var (
@@ -140,69 +208,75 @@ var (
 	rexBcid  = regexp.MustCompile("data-bcid=\"([^\"]+)\"")
 )
 
-func stopPlayer() {
-	pid := readInt("player-pid")
-	println("player pid:", pid)
+var pid int
+
+func stopPlayer() error {
+	log.Println("player pid:", pid)
 	if pid > 0 {
-		cmd := exec.Command("pkill", "omxplayer")
-		cmd.Run()
+		cmd := exec.Command("pkill", player)
+		err := cmd.Run()
 		time.Sleep(time.Second)
-		println("mplayer killed")
+		log.Println("kill signal sent")
+		return err
 	}
+	return nil
 }
 
-func playVideo(href string) {
-	stopPlayer()
+func startPlayer(href string) error {
+	err := stopPlayer()
+	if err != nil {
+		return err
+	}
 
 	resp, err := http.Get("http://www.smithsonianchannel.com" + href)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	buf, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	m := rexBcid.FindStringSubmatch(string(buf))
 	bcid := m[1]
 
-	n := readInt(bcid)
 	start := time.Now()
+	pos := state.Elapsed[bcid]
 	streamURL := fmt.Sprintf("http://c.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=%s&pubId=1466806621001", bcid)
 
-	//	cmd := exec.Command("mplayer", "-ss", strconv.Itoa(n), streamURL)
-	cmd := exec.Command("omxplayer", "--live", streamURL)
+	cmd := exec.Command(player, streamURL)
+	log.Printf("%+v\n", cmd.Args)
 
-	fmt.Printf("%+v\n", cmd.Args)
-
-	if err := cmd.Start(); err != nil {
-		panic(err)
+	if err = cmd.Start(); err != nil {
+		log.Println(err)
 	}
-	saveInt("player-pid", cmd.Process.Pid)
+	pid = cmd.Process.Pid
 
 	go func() {
-		err := cmd.Wait()
+		err = cmd.Wait()
 		if err != nil {
 			println(err.Error())
 		}
-		println("player stopped")
-		saveInt("player-pid", 0)
+		pid = 0
 		elapsed := time.Since(start)
-		if n+int(elapsed.Seconds()) > 20 {
-			saveInt(bcid, int(elapsed.Seconds())+n)
+		if elapsed > 20 {
+			state.Elapsed[bcid] = pos + int(elapsed.Seconds()) - 10
 		}
+		log.Println("player stopped. elapsed: ", elapsed)
+		saveState()
 	}()
+
+	return err
 }
 
 func getVideos() ([]item, error) {
-
 	resp, err := http.Get("http://www.smithsonianchannel.com/full-episodes")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	buf, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	s := string(buf)
@@ -234,7 +308,7 @@ func printList(n int) {
 	}
 
 	if n > 0 {
-		playVideo(list[n-1].Href)
+		startPlayer(list[n-1].Href)
 		return
 	}
 
@@ -245,17 +319,74 @@ func printList(n int) {
 
 var debug = flag.Bool("d", false, "debug in console")
 var playNum = flag.Int("p", 0, "play episode `NUM`")
+var cacheDir = os.Getenv("HOME") + "/.cache/stv"
+var configDir = os.Getenv("HOME") + "/.config/stv"
+var player = "mplayer"
+var playerArgs = []string{}
+var token string
+var tokenFname string
+var confapi string
+var addr = ":6061"
+
+func init() {
+	user := os.Getenv("USER")
+	if user == "pi" {
+		player = "omxplayer"
+	}
+	tokenFname = configDir + "/token.txt"
+}
+
+func createToken() string {
+	c := 10
+	b := make([]byte, c)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	s := hex.EncodeToString(b)
+	if err := ioutil.WriteFile(tokenFname, []byte(s), 0600); err != nil {
+		panic(err)
+	}
+	return s
+}
 
 func main() {
+	if err := os.MkdirAll(cacheDir, 0700); err != nil {
+		panic(err)
+	}
+	if err := os.MkdirAll(configDir, 0700); err != nil {
+		panic(err)
+	}
+
+	buf, err := ioutil.ReadFile(tokenFname)
+	token = string(buf)
+	if os.IsNotExist(err) || len(token) == 0 {
+		token = createToken()
+	} else if err != nil {
+		panic(err)
+	}
+	confapi = "https://conf.voilokov.com/" + token + "/stv/config"
+	loadState()
+
+	log.Println("player:", player, "token:", token)
+	addrs, _ := net.InterfaceAddrs()
+	for _, addr := range addrs {
+		log.Println("addr: ", addr.String())
+	}
+
 	flag.Parse()
 	if *debug {
 		printList(*playNum)
 		return
 	}
 
+	http.HandleFunc("/shutdown", shutdownHandler)
+	http.HandleFunc("/restart", restartHandler)
 	http.HandleFunc("/play", playHandler)
 	http.HandleFunc("/", mainHandler)
-	if err := http.ListenAndServe(":6061", nil); err != nil {
+
+	log.Println("serving: http://localhost" + addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
 		panic(err)
 	}
 }
